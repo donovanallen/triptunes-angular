c235f85041c85675b05ddf4eb2fc6053867e1f93
Donovan Allen <dallen.928@gmail.com>
2016-12-20 17:59:35 -0500

    adds some pazazz

--- app.js
+++ app.js
@@ -8,19 +8,25 @@ angular
   ])
   .controller("tripCtrl", [
     "$scope",
-    "$state"
+    "$state",
+    "$http",
     tripControllerFunction
   ])
-  // .factory("TripFactory",[
-  //   "$resource",
-  //   TripFactoryFunction
-  // ])
-  //
-  // function TripFactoryFunction($resource) {
-  //   return $resource("https://localhost/api/trip", {}, {
-  //     update: {method: "PUT"}
-  //   })
-  // }
+  .controller("showCtrl", [
+    "$scope",
+    "$state",
+    showControllerFunction
+  ])
+  .factory("TripFactory",[
+    "$resource",
+    TripFactoryFunction
+  ])
+
+  function TripFactoryFunction($resource) {
+    return $resource("https://localhost/api/trip", {}, {
+      update: {method: "PUT"}
+    })
+  }
 
 
   let tripData = [
@@ -46,8 +52,33 @@ angular
         controller: "tripCtrl",
         controllerAs: "vm"
       })
+      .state("show", {
+        url: "/trip",
+        templateUrl: "js/ng-views/show.html",
+        controller: "showCtrl",
+        controllerAs: "vm"
+      })
   }
 
-  function tripControllerFunction($scope, $state) {
+  function tripControllerFunction($scope, $state, $http) {
+    this.trips = tripData
+    
+    $scope.user = {
+      origin: "",
+      destination: ""
+    }
+
+    this.create = function(user) {
+      $http({
+        url: "http://localhost:4000/api/trip",
+        method: "post",
+        data: user
+      }).then((res) => {
+        $state.go("show", {}, {reload: true});
+      })
+    }
+  }
+
+  function showControllerFunction($scope, $state) {
     this.trips = tripData
   }
